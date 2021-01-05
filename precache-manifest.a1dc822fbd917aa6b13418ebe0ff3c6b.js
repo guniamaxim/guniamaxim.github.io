d6c71e1d2f96e94a6b1c602f351ee75038dc6c44
Gunya Maxim <52348667+guniamaxim@users.noreply.github.com>
2021-01-05 14:41:43 +0200

    Add files via upload

---
+++ precache-manifest.a1dc822fbd917aa6b13418ebe0ff3c6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feb95923eed97ee70fa6",
+    "url": "/css/app.ca936927.css"
+  },
+  {
+    "revision": "9fe787fee911f722ef2a7b43e5d2c97a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "feb95923eed97ee70fa6",
+    "url": "/js/app.9bf5c339.js"
+  },
+  {
+    "revision": "24fdcbbede81c0adf7a2",
+    "url": "/js/chunk-vendors.3aca0e4c.js"
+  },
+  {
+    "revision": "e39e0a53c64431bb773ee869b77e2fee",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
